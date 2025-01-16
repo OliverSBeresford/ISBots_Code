ee195984e606bb5b6dcff8a16c946648f1053f45
Oliver Beresford <118030320+OliverSBeresford@users.noreply.github.com>
2025-01-16 13:26:09 -0500

    Made a new static class to return Vision Components

--- src/robotUtils.java
+++ src/robotUtils.java
@@ -1,10 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.List;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 public class RobotUtils {
 
@@ -106,5 +118,65 @@ public class RobotUtils {
 
         leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    /**
+     * Initialize AprilTag Detection.
+     */
+    public VisionComponents initAprilTag(LinearOpMode opMode) {
+        // Variables to store the position and orientation of the camera on the robot. Setting these
+        // values requires a definition of the axes of the camera and robot:
+        // Camera axes:
+        // Origin location: Center of the lens
+        // Axes orientation: +x right, +y down, +z forward (from camera's perspective)
+        // Robot axes (this is typical, but you can define this however you want):
+        // Origin location: Center of the robot at field height
+        // Axes orientation: +x right, +y forward, +z upward
+        // Position:
+        // If all values are zero (no translation), that implies the camera is at the center of the
+        // robot. Suppose your camera is positioned 5 inches to the left, 7 inches forward, and 12
+        // inches above the ground - you would need to set the position to (-5, 7, 12).
+        // Orientation:
+        // If all values are zero (no rotation), that implies the camera is pointing straight up. In
+        // most cases, you'll need to set the pitch to -90 degrees (rotation about the x-axis), meaning
+        // the camera is horizontal. Use a yaw of 0 if the camera is pointing forwards, +90 degrees if
+        // it's pointing straight left, -90 degrees for straight right, etc. You can also set the roll
+        // to +/-90 degrees if it's vertical, or 180 degrees if it's upside-down.
+        // Position(DistanceUnit unit, double x, double y, double z, long acquisitionTime)
+        Position cameraPosition = new Position(DistanceUnit.CM, 5, 21.5, 18, 0);
+        // YawPitchRollAngles(AngleUnit angleUnit, double yaw, double pitch, double roll, long acquisitionTime)
+        YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES, 0, -90, 0, 0);
+
+        AprilTagProcessor.Builder myAprilTagProcessorBuilder;
+        VisionPortal.Builder myVisionPortalBuilder;
+
+        // First, create an AprilTagProcessor.Builder.
+        myAprilTagProcessorBuilder = new AprilTagProcessor.Builder();
+        myAprilTagProcessorBuilder.setCameraPose(cameraPosition, cameraOrientation);
+        
+        // Create an AprilTagProcessor by calling build.
+        // Create the AprilTag processor and assign it to a variable.
+        AprilTagProcessor myAprilTagProcessor = myAprilTagProcessorBuilder.build();
+
+        // Next, create a VisionPortal.Builder and set attributes related to the camera.
+        myVisionPortalBuilder = new VisionPortal.Builder();
+        myVisionPortalBuilder.setCamera(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        // Add myAprilTagProcessor to the VisionPortal.Builder.
+        myVisionPortalBuilder.addProcessor(myAprilTagProcessor);
+        // Create a VisionPortal by calling build.
+        VisionPortal myVisionPortal = myVisionPortalBuilder.build();
+        
+        return new VisionComponents(myVisionPortal, myAprilTagProcessor);
+    }
+
+    public static class VisionComponents {
+        VisionPortal visionPortal;
+        AprilTagProcessor aprilTagProcessor;
+
+        public VisionComponents(VisionPortal vision_portal, AprilTagProcessor apriltag_processor) {
+            visionPortal = vision_portal;
+            aprilTagProcessor = apriltag_processor; 
+        }
     }
 }
