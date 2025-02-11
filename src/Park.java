6a68e937eddf4f6d2c849a8c89829edd8bb4b7a9
Oliver Beresford <oggynaut@icloud.com>
2025-02-11 15:57:44 -0500

    Changed test code

--- src/Park.java
+++ src/Park.java
@@ -105,7 +105,10 @@ public class Park extends LinearOpMode {
 
         imu.resetYaw();
         robotUtils.turnDegrees(this, -90, true);
-        robotUtils.driveStraight(this, 60, 0.5, -90, true);
+        robotUtils.turnDegrees(this, 20, true);
+        robotUtils.turnDegrees(this, 70, true);
+        robotUtils.turnDegrees(this, -180, true);
+        // robotUtils.driveStraight(this, 60, 0.5, -90, true);
     }
 }
 
