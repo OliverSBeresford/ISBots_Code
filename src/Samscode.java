21dc19157ce6f05de118c3489dea698d4bf23416
seb102407 <sbiotti@isbos.org>
2025-02-25 16:45:04 -0500

    Made a New File :)

---
+++ src/Samscode.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.teamcode.RobotUtils;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import java.net.InterfaceAddress;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@TeleOp
+public class Samscode extends LinearOpMode {
+    /* Declare OpMode members. */
+    public DcMotor  leftDrive   = null; //the left drivetrain motor
+    public DcMotor  rightDrive  = null; //the right drivetrain motor
+    public DcMotor  armMotor    = null; //the arm motor
+    public CRServo  intake      = null; //the active intake servo
+    public Servo    wrist       = null; //the wrist servo
+    private ColorSensor colorSensor;
+    private RobotUtils robotUtils;
+
+    public void runOpMode() {
+
+        https://prod.liveshare.vsengsaas.visualstudio.com/join?97E6E18ADAE79DA2CA95ED05C4C9E1DC4246
+    }
